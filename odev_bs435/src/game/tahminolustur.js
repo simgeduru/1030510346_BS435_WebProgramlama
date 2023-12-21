a6c9c13c83eed4c05bda5ded234b29f5af74ac71
simgeduru <durusimge22@gmail.com>
2023-12-21 15:46:50 +0300

    Not eklendi tahminolustur

--- odev_bs435/src/game/tahminolustur.js
+++ odev_bs435/src/game/tahminolustur.js
@@ -2,6 +2,7 @@ export const rastgeleSayiUret = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+//delay ekle
 export const OyunModlari = {
     KOLAY: 'kolay',
     ZOR: 'zor',
